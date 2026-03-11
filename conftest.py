a9710848b3d5164089b3f4f8543b8a9a36a104db
Bonsa Desalegn <128582091+bonssss@users.noreply.github.com>
2026-03-11 13:12:28 +0300

    Configure browser launch headless mode based on CI

--- conftest.py
+++ conftest.py
@@ -12,7 +12,8 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def browser_page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        headless = True if os.getenv("CI") else False
+        browser = p.chromium.launch(headless=headless)
         context = browser.new_context()
         page = context.new_page()
         yield page
